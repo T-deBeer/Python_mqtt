72ed38fe1ed4a2a860a06c04ce84ac7ccd123874
T-deBeer <tdebeer.za@gmail.com>
2023-03-27 14:41:31 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+import time
+import paho.mqtt.client as mqtt
+
+broker = '41.193.5.155'
+port = 23000
+topic = "Topic/PhotoResistor"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = random.randint(0, 4096)
+        result = client.publish(topic, msg)
+        status = result[0]
+
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+            
+        msg_count += 1
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+
+if __name__ == '__main__':
+    run()
