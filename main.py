d1e8a7a3f020d1a6827043b5f76f78ed3134e31f
T-deBeer <tdebeer.za@gmail.com>
2023-03-29 15:39:20 +0200

    Massive Changes

--- main.py
+++ main.py
@@ -1,48 +1,60 @@
+from flask import Flask, render_template, request
+import paho.mqtt.publish as publish
 import random
 import time
-import paho.mqtt.client as mqtt
 
 broker = '41.193.5.155'
 port = 23000
-topic = "Topic/PhotoResistor"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
-
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-
-    client = mqtt.Client(client_id)
-    client.on_connect = on_connect
-
-    client.connect(broker, port)
-    return client
+app = Flask(__name__)
 
 
-# Publish to Dashboard aka Sending Data
-def publish(client):
-    while True:  # Loops infinitely
-        time.sleep(1)
-        msg = random.randint(0, 4096)  # Generates random Analog reading
-        # Sends the message(random integer) data to topic = 'Topic/PhotoResistor'
-        result = client.publish(topic, msg)
-        # Checks wheter or not the message was sent successfully
-        status = result[0]
-
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-def run():
-    client = connect_mqtt()
-    client.loop_start()
-    publish(client)
+@app.route('/', methods=['POST'])
+def form_data():
+    ph = request.form['ph']
+    water_level = request.form['water_level']
+    lux = request.form['lux']
+    temperature = request.form['temperature']
+    humidity = request.form['humidity']
+
+    ph_topic = 'Topic/pH'
+    wl_topic = 'Topic/WaterLevel'
+    lux_topic = 'Topic/PhotoResistor'
+    temp_topic = 'Topic/Temperature'
+    humid_topic = 'Topic/Humidity'
+
+    msgs = [{
+        'topic': ph_topic,
+        'payload': ph,
+        "qos": 2
+    }, {
+        'topic': wl_topic,
+        'payload': water_level,
+        "qos": 2
+    }, {
+        'topic': lux_topic,
+        'payload': lux,
+        "qos": 2
+    }, {
+        'topic': temp_topic,
+        'payload': temperature,
+        "qos": 2
+    }, {
+        'topic': humid_topic,
+        'payload': humidity,
+        "qos": 2
+    }]
+
+    publish.multiple(msgs, hostname=broker, port=port)
+    time.sleep(1)
+    return (render_template('index.html'))
 
 
 if __name__ == '__main__':
-    run()
+    app.run(host='0.0.0.0', port=8080)
